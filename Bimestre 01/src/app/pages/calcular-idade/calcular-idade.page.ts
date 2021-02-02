a69d61df27d64bf6fe632615abc501d03cee7087
Emanuelly <emanuellyvcarv@gmail.com>
2021-02-01 22:15:04 -0300

    Correção na lógica do typescript

--- Bimestre 01/src/app/pages/calcular-idade/calcular-idade.page.ts
+++ Bimestre 01/src/app/pages/calcular-idade/calcular-idade.page.ts
@@ -13,54 +13,72 @@ export class CalcularIdadePage implements OnInit {
   private idadeHoras: number;
   private idadeMinutos: number;
   private proximoAniversario: number;
-  private diaSemana: string;
+  private diaDaSemana: string;
 
   constructor() {
   }
 
   getDate(): string | Date {
-    const data = new Date();
-    let max = data.toLocaleString('default', {year: 'numeric', month: 'numeric', day: 'numeric'});
-    const fracao = max.split('/');
-    max = fracao[2] + '-' + fracao[1] + '-' + fracao[0];
-    return max;
+      const date = new Date();
+      let max = date.toLocaleString('default', {year: 'numeric', month: 'numeric', day: 'numeric'});
+      max = max.split('/')[2] + '-' + max.split('/')[1] + '-' + max.split('/')[0];
+      return max;
   }
 
-  async calcular(){
+  async calcular(): Promise<void> {
     const nasc = new Date(this.nascimento);
-    if(nasc != null){
-      this.calcularIdade(nasc);
-      this.calcularAniversario(nasc);
+      if (!isNaN(Number(nasc))) {
+          this.calcularIdade(nasc);
+          this.calcularAniversario(nasc);
+          return;          
+      }
+      alert('Insira uma data!');
       return;
-    }
-    alert('Por favor, insira uma data.');
-    return;
   }
 
   async calcularAniversario(nasc: Date){
-    const currentDate = new Date();
+    const dataAtual = new Date();
     const anoNasc = nasc.getFullYear();
-    const anoAtual = currentDate.getFullYear();
-    const bissexto = this.calcularBissexto(anoAtual, anoNasc);
+    const anoAtual = dataAtual.getFullYear();
+    const bissexto = this.calcularBissexto(anoNasc, anoAtual);
     this.proximoAniversario = this.calcularDias(anoNasc, bissexto);
-    this.diaSemana = this.calcularDiaDaSemana(nasc);
-    return;    
+    this.diaDaSemana = this.calcularDiaSemana(nasc, dataAtual);
+
   }
 
-  async calcularIdade(nasc: Date){
-    const currentDate = new Date();
-    this.idadeDias = Number(currentDate) - Number(nasc) / (24 * 3600 * 1000);
-    this.idadeHoras = this.idadeDias * 24;
-    this.idadeMinutos = this.idadeHoras * 60;
-    return;
+  calcularDiaSemana(nasc: Date, dataAtual: Date){
+    const mesNasc = nasc.getMonth();
+    const mesAtual = dataAtual.getMonth();
+    const diaNasc = nasc.getDate();
+    const diaAtual = dataAtual.getDate();
+    const anoAtual = dataAtual.getFullYear();
+    let dia: number;
+
+    if (mesNasc === mesAtual && diaNasc <= diaAtual) {
+      dia = new Date(anoAtual + 1, mesAtual, diaNasc).getDay();
+      return this.diaSemana(dia);
+    } 
+
+    if (mesNasc === mesAtual && diaNasc > diaAtual) {
+      dia = new Date(anoAtual, mesNasc, diaNasc).getDay();
+      return this.diaSemana(dia);
+    }
+    
+    if (mesNasc < mesAtual) {
+      dia = new Date(anoAtual + 1, mesNasc, diaNasc).getDay();
+      return this.diaSemana(dia);
+    } 
+    
+    dia = new Date(anoAtual, mesNasc, diaNasc).getDay();
+    return this.diaSemana(dia);
   }
 
-  calcularBissexto(anoAtual, anoNasc){
+  calcularBissexto(anoNasc: number, anoAtual: number){
     let bissexto = 0;
     for (let i = 0; i < anoAtual - anoNasc; i++) {
-      if ((anoNasc + i) % 4 === 0) {
-        bissexto += 1;
-      }
+        if ((anoNasc + i) % 4 === 0) {
+            bissexto += 1;
+        }
     }
     return bissexto;
   }
@@ -82,49 +100,31 @@ export class CalcularIdadePage implements OnInit {
     return diasFaltantes;
   }
 
-  calcularDiaDaSemana(nasc: Date){
-    const currentDate = new Date();
-    const anoAtual = currentDate.getFullYear();
-    const mesNasc = new Date(this.nascimento).getMonth();
-    const mesAtual = currentDate.getMonth(); 
-    const dataNasc =  nasc.getDate();
-    const dataAtual = currentDate.getDate();
-    let dia: number;
-    
-    if (mesNasc > mesAtual) {
-      dia = new Date(anoAtual, mesNasc, dataNasc).getDay()
-    } 
-    
-    if (mesNasc === mesAtual && dataNasc <= dataAtual) {
-      dia = new Date(anoAtual + 1, mesAtual, dataNasc).getDay();
-    }
-    
-    if (mesNasc === mesAtual && dataNasc > dataAtual) {
-      dia = new Date(anoAtual, mesNasc, dataNasc).getDay();
-    }
-
-    return this.diaDaSemana(dia);
+  async calcularIdade(nasc: Date){
+    const dataAtual = new Date();
+    this.idadeDias = parseInt(String((Number(dataAtual) - Number(nasc)) / (24 * 3600 * 1000)));
+    this.idadeHoras = this.idadeDias * 24;
+    this.idadeMinutos = this.idadeHoras * 60;
+    return;
   }
 
-  diaDaSemana(dia: number){
-    switch (dia) {
-      case 0:
-        return 'Domingo';
-      case 1:
-        return 'Segunda-feira';
-      case 2:
-        return 'Terça-feira';
-      case 3:
-        return 'Quarta-feira';
-      case 4:
-        return 'Quinta-feira';
-      case 5:
-        return 'Sexta-feira';
-      case 6:
-        return 'Sábado';
-      default:
-        return 'Indefinido';
-    }
+  diaSemana(day: number): string {
+      switch (day) {
+          case 0:
+              return 'Domingo';
+          case 1:
+              return 'Segunda-feira';
+          case 2:
+              return 'Terça-feira';
+          case 3:
+              return 'Quarta-feira';
+          case 4:
+              return 'Quinta-feira';
+          case 5:
+              return 'Sexta-feira';
+          case 6:
+              return 'Sábado';
+      }
   }
 
   ngOnInit() {
